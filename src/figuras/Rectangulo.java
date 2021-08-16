6babf1c5dd5dc554174f6eb5fa11e19af611e0db
Luciano <Luciano@DESKTOP-BHTTITI>
2021-08-16 15:27:37 -0300

    Versión inicial del proyecto figuras

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,15 @@
+package figuras;
+
+public class Rectangulo {
+	
+	private double alto, ancho;
+
+	public Rectangulo(double alto, double ancho) {
+
+		this.alto = alto;
+		this.ancho = ancho;
+	}
+	
+	
+	
+}
